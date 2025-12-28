7a86d419c6d4b8807634a5bbc57ca9c0382b9cdc
Павел Калабин <kalabinpavel57@gmail.com>
2025-12-28 20:04:15 +0300

    Border Warning script & prefab

---
+++ Assets/Scripts/GameUI/BorderWarningController.cs
@@ -0,0 +1,31 @@
+using Mirror;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BorderWarningController : MonoBehaviour
+{
+	[Header("UI Elements")]
+    [SerializeField] private GameObject panel;
+    [SerializeField] private TextMeshProUGUI warningText;
+	
+    [Header("Killfeed Settings")]
+    [SerializeField] private Transform warningContainer;
+    [SerializeField] private GameObject warningItemPrefab;
+	
+	private GameObject item;
+	
+	public void Show()
+    {
+        item = Instantiate(warningItemPrefab, warningContainer);
+        var textComp = item.GetComponentInChildren<TextMeshProUGUI>();
+        string message = $"Warning: Space Radiation";
+	
+        textComp.text = message;
+    }
+	
+	public void Hide()
+    {
+        Destroy(item, 0);
+    }
+}
