b66a992faad37ae45e34109452cb480334a2ba53
Шевцов Антон <n3ktar56@gmail.com>
2026-02-07 10:50:29 +0300

    Fix local leaderboard blocking respawn UI on death

--- Assets/Scripts/GameUI/UIManager.cs
+++ Assets/Scripts/GameUI/UIManager.cs
@@ -35,6 +35,8 @@ public class UIManager : MonoBehaviour
 
     public void ShowDeathScreen(DamageContext source)
     {
+        SetLeaderboardVisible(false);
+
         deathScreenController.Show(source);
     }
 
